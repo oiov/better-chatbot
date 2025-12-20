324a01009ad8d073287d90cacb9f15c7e09018c7
oiov <hi@oiov.dev>
2025-12-20 13:40:11 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,6 +49,14 @@ export default async function RootLayout({
                 {children}
                 <Toaster richColors />
               </div>
+              <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-22RPQ62H4E"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-22RPQ62H4E');
+</script>
             </NextIntlClientProvider>
           </ThemeStyleProvider>
         </ThemeProvider>
